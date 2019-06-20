136365374211ef5a64c86433591e4d5f7a673d1f
Artem S1lentem <tema.yarmosh@gmail.com>
2019-06-20 11:54:49 +0300

    develop base repo

---
+++ HiQoDataGenerator.DAL/Repositories/EntityFramework/TimezonesRepository.cs
@@ -0,0 +1,43 @@
+﻿using HiQoDataGenerator.DAL.Contracts.Repositories;
+using HiQoDataGenerator.DAL.Models.ConstraintModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HiQoDataGenerator.DAL.Repositories.EntityFramework
+{
+    class TimezonesRepository : IBaseRepository<Timezone>
+    {
+        private readonly DataContext _context;
+
+        public TimezonesRepository(DataContext context) => _context = context;
+
+        public void Add(Timezone item)
+        {
+            _context.Timezones.Add(item);
+            _context.SaveChangesAsync();
+        }
+
+        public void AddRange(IEnumerable<Timezone> items)
+        {
+            _context.Timezones.AddRange(items);
+            _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Timezone> GetAll() => _context.Timezones;
+
+        public Timezone GetById(int id) => _context.Timezones.Find(id);
+
+        public bool RemoveById(int id)
+        {
+            var valueForRemove = GetById(id);
+            if (valueForRemove == null)
+            {
+                return false;
+            }
+            _context.Remove(valueForRemove);
+            return true;
+        }
+    }
+}
