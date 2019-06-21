1dd75eaf0efbfb045d8d96ab26387cb37ce3eab1
Artem Yarmosh <tema.yarmosh@gmail.com>
2019-06-21 20:20:25 +0300

    Added factory for mapper's (#18)
    * Completed develop factory for mapper's
    
    * Update MapperFactory.cs
    
    rename factoryName to mapperName
    
    * Update IMapperFactory.cs

---
+++ HiQoDataGenerator.Core/Interfaces/IMapperFactory.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiQoDataGenerator.Core.Interfaces
+{
+    public interface IMapperFactory
+    {
+        IMapper GetMapper(string mapperName);
+    }
+}
---
+++ HiQoDataGenerator.Web/WebServices.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HiQoDataGenerator.Core;
+using HiQoDataGenerator.Core.Extensions;
+using HiQoDataGenerator.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HiQoDataGenerator.Web.Extensions
+{
+    public static class WebServices
+    {
+        public static void AddMapperFactory(this IServiceCollection services)
+        {
+            var factory = new MapperFactory();
+
+            factory.Mappers.Add(typeof(CoreServices).Name, CoreServices.GetMapper());
+            factory.Mappers.Add(typeof(WebServices).Name, WebServices.GetMapper());
+
+            services.AddSingleton<IMapperFactory>(factory);
+        }
+
+
+        public static IMapper GetMapper() => MapperExtension.GetMapper();
+    }
+}
