4192b1f335a3bd15b5dc809bf212c88bd3b85d47
DenisKaminsky <36419821+DenisKaminsky@users.noreply.github.com>
2019-06-21 20:12:10 +0300

    Update MapperFactory.cs
    rename factoryName to mapperName

--- HiQoDataGenerator.Core/Extensions/MapperFactory.cs
+++ HiQoDataGenerator.Core/Extensions/MapperFactory.cs
@@ -9,6 +9,6 @@ namespace HiQoDataGenerator.Core.Extensions
     public class MapperFactory : IMapperFactory
     {
         public Dictionary<string, IMapper> Mappers { get; set; } = new Dictionary<string, IMapper>();
-        public IMapper GetMapper(string factoryName = "") => Mappers[factoryName];
+        public IMapper GetMapper(string mapperName = "") => Mappers[mapperName];
     }
 }
