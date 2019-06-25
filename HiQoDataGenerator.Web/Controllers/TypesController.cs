798d570c4b65043bf6744493782e237aa945964b
DenisKaminsky <deniskaminsky123@mail.ru>
2019-06-25 13:43:55 +0300

    add Field Types tests and remove logs from typesController

--- HiQoDataGenerator.Web/Controllers/TypesController.cs
+++ HiQoDataGenerator.Web/Controllers/TypesController.cs
@@ -5,25 +5,22 @@ using Microsoft.AspNetCore.Mvc;
 using HiQoDataGenerator.Core.Interfaces;
 using HiQoDataGenerator.Core.Entities;
 using HiQoDataGenerator.Web.ViewModels;
-using System.Net;
-using Microsoft.Extensions.Logging;
+using HiQoDataGenerator.Web.Attributes;
 
 namespace HiQoDataGenerator.Web.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ServiceFilter(typeof(LoggingAttribute))]
     public class TypesController : ControllerBase
     {
         private readonly IFieldTypeService _fieldTypesService;
         private readonly IMapper _mapper;
-        private readonly ILogger _logger;
-        private readonly string _loggerName = "FieldTypesLogger";
 
-        public TypesController(IFieldTypeService fieldTypesService,IMapperFactory mapperFactory,ILoggerFactory loggerFactory)
+        public TypesController(IFieldTypeService fieldTypesService,IMapperFactory mapperFactory)
         {
             _fieldTypesService = fieldTypesService;
             _mapper = mapperFactory.GetMapper(typeof(WebServices).Name);
-            _logger = loggerFactory.CreateLogger(_loggerName);
         }
         
         [HttpGet]
@@ -31,38 +28,30 @@ namespace HiQoDataGenerator.Web.Controllers
         {
             var typeModels = _fieldTypesService.GetAll();
             var typeViewModels = _mapper.Map<IEnumerable<FieldTypeViewModel>>(typeModels);
-            _logger.LogInformation("Get all Types");
             return Ok(typeViewModels);
         }
         
         [HttpGet("{id}")]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> Get(int id)
         {
             var typeModel = await _fieldTypesService.GetByIdAsync(id);
             var typeViewModel = _mapper.Map<FieldTypeViewModel>(typeModel);
-            _logger.LogInformation("Get Type with id {0}",typeViewModel.Id);
             return Ok(typeViewModel);
         }
         
         [HttpPost]
-        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> Post(FieldTypeViewModel typeViewModel)
         {
             var typeModel = _mapper.Map<FieldTypeModel>(typeViewModel);
 
             await _fieldTypesService.AddAsync(typeModel);
-            _logger.LogInformation("Add Type {0}",typeViewModel.Name);
             return Ok(typeModel);
         }
                 
         [HttpDelete("{id}")]
-        [ProducesResponseType((int)HttpStatusCode.NoContent)]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> Delete(int id)
         {
-            await _fieldTypesService.RemoveByIdAsync(id);            
-            _logger.LogInformation("Delete Type with id {0}", id);
+            await _fieldTypesService.RemoveByIdAsync(id);          
             return NoContent();
         }
     }
