e777e33f430c42aeb11d01b05649dcab370222dc
DenisKaminsky <36419821+DenisKaminsky@users.noreply.github.com>
2019-06-25 13:23:00 +0300

    Denis date format tests (#38)
    * add test
    
    * remove logs from dateTimeFormatController
    
    * add getAll and getById tests
    
    * add removeById tests
    
    * Add AddAsync test

---
+++ HiQoDataGenerator.Tests/HiQoDataGenerator.Core/Services/DateTimeFormatTest.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using HiQoDataGenerator.Core;
+using HiQoDataGenerator.Core.Interfaces;
+using HiQoDataGenerator.Core.Services;
+using HiQoDataGenerator.DAL.Contracts.Repositories;
+using HiQoDataGenerator.DAL.Models.ConstraintModels;
+using HiQoDataGenerator.Core.Exceptions;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using System.Collections.Generic;
+
+namespace HiQoDataGenerator.Tests.HiQoDataGenerator.Core.Services
+{
+    public class DateTimeFormatTest
+    {       
+        private readonly IMapper _mapper;
+        private readonly Mock<IMapperFactory> _mapperFactoryMock;
+        private readonly Mock<IDateTimeFormatRepository> _repositoryMock;
+        private readonly List<DateTimeFormat> _dateTimeFormats;
+        private readonly IDateTimeFormatService _dateTimeFormatService;
+
+        public DateTimeFormatTest()
+        {
+            _mapper = CoreServices.GetMapper();
+            _repositoryMock = new Mock<IDateTimeFormatRepository>();
+            _mapperFactoryMock = new Mock<IMapperFactory>();
+            _dateTimeFormats = GenerateDateTimeFormats();
+
+            ConfigureRepositoryMock(_repositoryMock);
+            ConfigureMapperFactoryMock(_mapperFactoryMock);
+            _dateTimeFormatService = new DateTimeFormatService(_repositoryMock.Object, _mapperFactoryMock.Object);
+        }
+        
+        private List<DateTimeFormat> GenerateDateTimeFormats()
+        {
+            return new List<DateTimeFormat>() { new DateTimeFormat() { Id = 1 }, new DateTimeFormat() { Id = 2 } };
+        }
+
+        private void ConfigureMapperFactoryMock(Mock<IMapperFactory> mapperFactoryMock)
+        {
+            mapperFactoryMock.Setup(factory => factory.GetMapper(typeof(CoreServices).Name)).Returns(() => _mapper);
+        }
+
+        private void ConfigureRepositoryMock(Mock<IDateTimeFormatRepository> repositoryMock)
+        {
+            repositoryMock.Setup(rep => rep.GetAll()).Returns(_dateTimeFormats.AsQueryable());
+            repositoryMock.Setup(rep => rep.GetByIdAsync(1)).Returns(async () => _dateTimeFormats[0]);
+            repositoryMock.Setup(rep => rep.GetByIdAsync(3)).Returns(async () => null);
+
+            repositoryMock.Setup(rep => rep.AddAsync(null)).Returns(async () => true);
+
+            repositoryMock.Setup(rep => rep.RemoveByIdAsync(1)).Returns(async () => true);
+            repositoryMock.Setup(rep => rep.RemoveByIdAsync(3)).Returns(async () => false);
+        }
+
+        [Fact]
+        public void GetAll_RightDateTimeFromatsCount()
+        {
+            var result = _dateTimeFormatService.GetAll();
+
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public async void GetByIdAsync_ExistingId_RightDateTimeFromat()
+        {
+            var result = await _dateTimeFormatService.GetByIdAsync(1);
+
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_NonExistingId_ElementNotFoundException()
+        {
+            await Assert.ThrowsAsync<ElementNotFoundException>(() => _dateTimeFormatService.GetByIdAsync(3));
+        }
+
+        [Fact]
+        public async Task AddAsync_NonExistingElement_NoException()
+        {
+            var isNotThrown = true;
+            try
+            {
+                await _dateTimeFormatService.AddAsync(null);
+            }
+            catch (ElementIsAlreadyExistException)
+            {
+                isNotThrown = false;
+            }
+            Assert.True(isNotThrown);
+        }
+
+        [Fact]
+        public async Task RemoveByIdAsync_ExistingId_NoException()
+        {
+            var isNotThrown = true;
+            try
+            {
+                await _dateTimeFormatService.RemoveByIdAsync(1);
+            }
+            catch(ElementNotFoundException)
+            {
+                isNotThrown = false;
+            }
+            Assert.True(isNotThrown);
+        }
+
+        [Fact]
+        public async Task RemoveByIdAsync_NonExistingId_ElementNotFoundException()
+        {
+            await Assert.ThrowsAsync<ElementNotFoundException>(() => _dateTimeFormatService.RemoveByIdAsync(3));
+        }
+    }
+}
