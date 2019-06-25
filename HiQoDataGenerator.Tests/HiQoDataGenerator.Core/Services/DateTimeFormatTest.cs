0aa81ee469ea6ec35ed5a16f89b2ea0f4cab9b2a
DenisKaminsky <deniskaminsky123@mail.ru>
2019-06-25 12:43:16 +0300

    add getAll and getById tests

--- HiQoDataGenerator.Tests/HiQoDataGenerator.Core/Services/DateTimeFormatTest.cs
+++ HiQoDataGenerator.Tests/HiQoDataGenerator.Core/Services/DateTimeFormatTest.cs
@@ -5,54 +5,72 @@ using HiQoDataGenerator.Core.Services;
 using HiQoDataGenerator.DAL.Contracts.Repositories;
 using HiQoDataGenerator.DAL.Models.ConstraintModels;
 using HiQoDataGenerator.Core.Entities;
+using System;
+using System.Linq;
 using Moq;
 using Xunit;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace HiQoDataGenerator.Tests.HiQoDataGenerator.Core.Services
 {
     public class DateTimeFormatTest
-    {
-        private readonly Mock<IDateTimeFormatRepository> _repositoryMock;
+    {       
+        private readonly IMapper _mapper;
         private readonly Mock<IMapperFactory> _mapperFactoryMock;
-        private readonly Mock<IMapper> _mapperMock;
-
+        private readonly Mock<IDateTimeFormatRepository> _repositoryMock;
+        private readonly List<DateTimeFormat> _dateTimeFormats;
         private readonly IDateTimeFormatService _dateTimeFormatService;
 
         public DateTimeFormatTest()
         {
-            _mapperMock = new Mock<IMapper>();
-            _mapperFactoryMock = new Mock<IMapperFactory>();
+            _mapper = CoreServices.GetMapper();
             _repositoryMock = new Mock<IDateTimeFormatRepository>();
+            _mapperFactoryMock = new Mock<IMapperFactory>();
+            _dateTimeFormats = GenerateDateTimeFormats();
 
-            ConfigureMapperMock(_mapperMock);
-            _mapperFactoryMock.Setup(factory => factory.GetMapper(typeof(CoreServices).Name)).Returns(() => _mapperMock.Object);
-            _dateTimeFormatService = new TimezoneService(_repositoryMock.Object, _mapperFactoryMock.Object);
+            ConfigureRepositoryMock(_repositoryMock);
+            ConfigureMapperFactoryMock(_mapperFactoryMock);
+            _dateTimeFormatService = new DateTimeFormatService(_repositoryMock.Object, _mapperFactoryMock.Object);
+        }
+        
+        private List<DateTimeFormat> GenerateDateTimeFormats()
+        {
+            return new List<DateTimeFormat>() { new DateTimeFormat() { Id = 1 }, new DateTimeFormat() { Id = 2 } };
         }
 
-        private void ConfigureMapperMock(Mock<IMapper> mapperMock)
+        private void ConfigureMapperFactoryMock(Mock<IMapperFactory> mapperFactoryMock)
         {
-            mapperMock.Setup(mapper => mapper.Map<DateTimeFormat>(null))
+            mapperFactoryMock.Setup(factory => factory.GetMapper(typeof(CoreServices).Name)).Returns(() => _mapper);
+        }
+
+        private void ConfigureRepositoryMock(Mock<IDateTimeFormatRepository> repositoryMock)
+        {
+            repositoryMock.Setup(rep => rep.GetAll()).Returns(_dateTimeFormats.AsQueryable());
+            repositoryMock.Setup(rep => rep.GetByIdAsync(1)).Returns(async () => _dateTimeFormats[0]);
+            repositoryMock.Setup(rep => rep.GetByIdAsync(3)).Returns(async () => null);
+
+            repositoryMock.Setup(rep => rep.AddAsync(null)).Returns(async () => true);
+            repositoryMock.Setup(rep => rep.GetByIdAsync(3)).Returns(async () => throw new DbUpdateException("error",new Exception()));
+
+            repositoryMock.Setup(rep => rep.RemoveByIdAsync(1)).Returns(async () => true);
+            repositoryMock.Setup(rep => rep.RemoveByIdAsync(3)).Returns(async () => false);
         }
 
         [Fact]
-        public async void AddAsyncReturnedTrueIfRepositoryReturnedTrue()
+        public void DateTimeFormatService_GetAll_RightDateTimeFromatsCount()
         {
-            _repositoryMock.Setup(repository => repository.AddAsync(null)).Returns(async () => true);
+            var result = _dateTimeFormatService.GetAll();
 
-            bool isAdded = await _dateTimeFormatService.AddAsync(null);
-
-            Assert.True(isAdded);
+            Assert.Equal(2, result.Count());
         }
 
         [Fact]
-        public async void RemoveByIdReturnedFalseIfRepositoryReturnedFalse()
+        public async void DateTimeFormatService_GetById_RightDateTimeFromat()
         {
-            const int id = 1;
-            _repositoryMock.Setup(repository => repository.RemoveByIdAsync(id)).Returns(async () => false);
+            var result = await _dateTimeFormatService.GetByIdAsync(1);
 
-            bool isRemoved = await _dateTimeFormatService.RemoveByIdAsync(id);
-
-            Assert.False(isRemoved);
+            Assert.Equal(1, result.Id);
         }
     }
 }
