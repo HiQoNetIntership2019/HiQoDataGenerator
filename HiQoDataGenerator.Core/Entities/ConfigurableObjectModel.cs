046230da1189ed5c13afc7e313b96d475ce84346
Artem S1lentem <tema.yarmosh@gmail.com>
2019-06-26 12:03:07 +0300

    Added logic in model

--- HiQoDataGenerator.Core/Entities/ConfigurableObjectModel.cs
+++ HiQoDataGenerator.Core/Entities/ConfigurableObjectModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HiQoDataGenerator.Core.Entities
 {
-    public class ConfigurableObjectModel
+    public class ConfigurableObjectModel : BaseModel
     {
+        private string _name;
+
+        public DateTime DateCreation { get; }
+        public DateTime? DateChange { get; private set; }
+
+        public string Name {
+            get => _name;
+            set
+            {
+                _name = value;
+                DateChange = DateTime.Now;
+            }
+        }
+
+        public ConfigurableObjectModel(int id, string name, DateTime dateCreation, DateTime? dateChange) : base(id)
+        {
+            _name = name;
+            DateChange = dateChange;
+            DateCreation = dateCreation;
+        }
+
+        public ConfigurableObjectModel(int id, string name) : this(id, name, DateTime.Now, null) { }
     }
 }
