f1f06d29d8f6ab91670cdf273c32d1dbe4f2aefd
DenisKaminsky <deniskaminsky123@mail.ru>
2019-06-26 16:42:14 +0300

    Fix services

--- HiQoDataGenerator.Core/Services/DatasetTypesService.cs
+++ HiQoDataGenerator.Core/Services/DatasetTypesService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HiQoDataGenerator.Core.Entities;
 using HiQoDataGenerator.Core.Interfaces;
+using HiQoDataGenerator.Core.UnitOfWork;
 using HiQoDataGenerator.DAL.Contracts.Repositories;
 using HiQoDataGenerator.DAL.Models.DataSetModels;
 using System;
@@ -14,22 +15,32 @@ namespace HiQoDataGenerator.Core.Services
     {
         private readonly IMapper _mapper;
         private readonly IDatasetTypesRepository _datasetTypesRepository;
-        public DatasetTypesService(IMapperFactory mapperFactory, IDatasetTypesRepository datasetTypesRepository)
+        private readonly IUnitOfWork _uow;
+
+        public DatasetTypesService(IUnitOfWork unit, IMapperFactory mapperFactory, IDatasetTypesRepository datasetTypesRepository)
         {
+            _uow = unit;
             _mapper = mapperFactory.GetMapper(typeof(CoreServices).Name);
             _datasetTypesRepository = datasetTypesRepository;
         }
 
-        public async Task AddTypeAsync(DatasetTypeModel datasetTypeModel) => 
+        public async Task AddTypeAsync(DatasetTypeModel datasetTypeModel)
+        {
             await _datasetTypesRepository.AddAsync(_mapper.Map<DatasetType>(datasetTypeModel));
+            await _uow.CommitAsync();
+        }
 
 
-        public IEnumerable<DatasetTypeModel> GetAllTypes() => 
+        public IEnumerable<DatasetTypeModel> GetAllTypes() =>
             _mapper.Map<IEnumerable<DatasetTypeModel>>(_datasetTypesRepository.GetAll());
 
         public async Task<DatasetTypeModel> GetByIdAsync(int id) =>
             _mapper.Map<DatasetTypeModel>(await _datasetTypesRepository.GetByIdAsync(id));
 
-        public async Task RemoveById(int id) => await _datasetTypesRepository.RemoveByIdAsync(id);
+        public async Task RemoveById(int id)
+        {
+            await _datasetTypesRepository.RemoveByIdAsync(id);
+            await _uow.CommitAsync();
+        }
     }
 }
