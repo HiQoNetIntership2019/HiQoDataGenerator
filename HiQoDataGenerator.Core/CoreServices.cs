cb850640025681abb8c741dea741493a41eaf968
Darya <makarenko.darya.work@gmail.com>
2019-07-01 18:11:36 +0300

    add scrutor to core services

--- HiQoDataGenerator.Core/CoreServices.cs
+++ HiQoDataGenerator.Core/CoreServices.cs
@@ -3,8 +3,8 @@ using HiQoDataGenerator.Core.Extensions;
 using HiQoDataGenerator.Core.Interfaces;
 using HiQoDataGenerator.Core.Services;
 using HiQoDataGenerator.Core.UnitOfWork;
-using HiQoDataGenerator.DAL.Contracts.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
 
 namespace HiQoDataGenerator.Core
 {
@@ -12,20 +12,14 @@ namespace HiQoDataGenerator.Core
     {
         public static void AddBLServices(this IServiceCollection services)
         {
-            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            services.Scan(sc =>
+                sc.FromCallingAssembly()
+                    .AddClasses(classes => classes.InNamespaceOf<ConstraintsService>())
+                    .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
 
-            services.AddScoped<ITimezonesService, TimezoneService>();
-            services.AddScoped<IRegexService, RegexService>();
-            services.AddScoped<IEncodingTypesService, EncodingTypeService>();
-            services.AddScoped<IRegexService, RegexService>();
-            services.AddScoped<IFieldTypeService, FieldTypeService>();
-            services.AddScoped<IConstraintsService, ConstraintsService>();
-            services.AddScoped<IDateTimeFormatService, DateTimeFormatService>();
-            services.AddScoped<IConfigurableObjectsService, ConfigurableObjectsService>();
-            services.AddScoped<IDatasetTypesService, DatasetTypesService>();
-            services.AddScoped<ICustomDatasetService, CustomDatasetService>();
-            services.AddScoped<IConstraintValuesService, ConstraintValuesService>();
-            services.AddScoped<IFieldsService, FieldsService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
         }
 
         public static IMapper GetMapper() => MapperExtension.GetMapper();
