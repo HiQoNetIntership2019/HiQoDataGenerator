b54ba0f1022f3944d5fd4b0a598e4efdb22c2b67
Artem S1lentem <tema.yarmosh@gmail.com>
2019-07-05 14:29:02 +0300

    Added abstract logic and tests for data generation

---
+++ HiQoDataGenerator.GeneratorCore/Exceptions/NegativeCountObjectsException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HiQoDataGenerator.GeneratorCore.Exceptions
+{
+    public class NegativeCountObjectsException : Exception
+    {
+        public NegativeCountObjectsException() : base("Count objects can not be negative") { }
+    }
+}
---
+++ HiQoDataGenerator.GeneratorCore/Models/Objects/GeneratedObject.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace HiQoDataGenerator.GeneratorCore.Models.Objects
+{
+    public class GeneratedObject
+    {
+        public string Name { get; }
+        public DateTime DateCreated { get; }
+        public ICollection<(string Name, object Value)> Fields { get; }
+
+
+        public GeneratedObject(string name, ICollection<(string, object)> fields = null)
+        {
+            Name = name;
+            DateCreated = DateTime.Now;
+            Fields = fields ?? new List<(string, object)>();
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
