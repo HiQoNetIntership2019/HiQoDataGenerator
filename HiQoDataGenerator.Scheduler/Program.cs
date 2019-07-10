e476a9489099920bc5fd2b821003381d5e1de308
DenisKaminsky <deniskaminsky123@mail.ru>
2019-07-10 17:13:42 +0300

    add scheduler

---
+++ HiQoDataGenerator.Scheduler/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using HiQoDataGenerator.Scheduler.Services;
+using HiQoDataGenerator.Scheduler.Extensions;
+using System.Threading.Tasks;
+
+namespace HiQoDataGenerator.Scheduler
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+                       
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<SchedulerHostedService>();
+                });
+
+            if (isService)
+            {
+                await builder.RunAsServiceAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+        }
+    }
+}
---
+++ HiQoDataGenerator.Scheduler/Services/SchedulerHostedService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HiQoDataGenerator.Scheduler.Services
+{
+    internal class SchedulerHostedService : IHostedService, IDisposable
+    {
+        //private readonly ILogger _logger;
+        private Timer _timer;
+
+        public SchedulerHostedService()
+        {
+           // _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            //_logger.LogInformation("Timed Background Service is starting.");
+            Console.WriteLine("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(10));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            //_logger.LogInformation("Timed Background Service is working.");
+            Console.WriteLine("Timed Background Service is working.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            //_logger.LogInformation("Timed Background Service is stopping.");
+            Console.WriteLine("Timed Background Service is stopping.");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
