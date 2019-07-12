f2c90f43ecf4d5a3c76f47b165b5f4212cf48300
Artem S1lentem <tema.yarmosh@gmail.com>
2019-07-12 11:46:19 +0300

    Added mongo and file system repositories

---
+++ HiQoDataGenerator.DAL/Models/GeneratedObjects/CustomGeneratedField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiQoDataGenerator.DAL.Models.GeneratedObjects
+{
+    public class CustomGeneratedField
+    { 
+        public string Name { get; set; }
+        public dynamic Value { get; set; }
+    }
+}
---
+++ HiQoDataGenerator.DAL/Repositories/FileSystem/FilesGeneratedObjectsRepository .cs
@@ -0,0 +1,54 @@
+﻿using HiQoDataGenerator.DAL.Contracts.Repositories;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HiQoDataGenerator.DAL.Repositories.MongoDirver
+{
+    public class FilesGeneratedObjectsRepository : IFilesGeneratedObjectsRepository
+    {
+        private readonly string _rootDirectory;
+
+        public FilesGeneratedObjectsRepository(IConfiguration configuration)
+        {
+            _rootDirectory = configuration["FileSystem:Root"];
+        }
+
+        public async Task CreateAndWriteInFile(string value, string directoryName, string fileName)
+        {
+            var fullPath = Path.Combine(_rootDirectory, directoryName, fileName);
+            File.Create(fullPath);
+
+            using (var writer = new StreamWriter(fullPath))
+            {
+                await writer.WriteAsync(value);
+            }
+        }
+
+        public async Task DeleteFile(string directoryName, string fileName)
+        {
+            await Task.Run(() => File.Delete(Path.Combine(_rootDirectory, directoryName, fileName)));
+        }
+
+        public async Task DeleteFile(string fullPath)
+        {
+            await Task.Run(() => File.Delete(fullPath));
+        }
+
+        public async Task<string> ReadFromFile(string directoryName, string fileName)
+        {
+            using (var reader = new StreamReader(Path.Combine(_rootDirectory, directoryName, fileName)))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        public async Task<string> ReadFromFile(string fullPath)
+        {
+            using (var reader = new StreamReader(fullPath))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
