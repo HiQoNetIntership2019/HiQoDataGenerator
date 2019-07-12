dc285b021b3db694c62e26aea97eea1be4f6b430
DenisKaminsky <deniskaminsky123@mail.ru>
2019-07-12 15:15:01 +0300

    fix

---
+++ HiQoDataGenerator.EventBus/Events/TimerEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace HiQoDataGenerator.EventBus.Events
+{
+    public class TimerEvent : BusEvent
+    {
+        public TimerEvent(string description) : base(description) { }
+    }
+}
--- HiQoDataGenerator.Scheduler/Services/SchedulerHostedService.cs
+++ HiQoDataGenerator.Scheduler/Services/SchedulerHostedService.cs
@@ -2,27 +2,36 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using HiQoDataGenerator.EventBus.EventBuses.RabbitMqEventBus;
+using HiQoDataGenerator.EventBus.Events;
 
 namespace HiQoDataGenerator.Scheduler.Services
 {
     public class SchedulerHostedService : IDisposable
     {
         private Timer _timer;
+        private RabbitMqEventBus _bus;
+        private TimerEvent _event;
 
-        public SchedulerHostedService() { }
+        public SchedulerHostedService()
+        {
+            _bus = new RabbitMqEventBus(GetType().Name);
+            _event = new TimerEvent("DoWork");
+        }
 
         public Task StartAsync()
         {
             Log.Information("Service started");
-
+            
             _timer = new Timer(PublishEvent, null, TimeSpan.Zero,
                 TimeSpan.FromSeconds(10));
 
             return Task.CompletedTask;
         }
 
-        private void PublishEvent(object busEvent)
+        private void PublishEvent(object state)
         {
+            _bus.Publish(_event);
             Log.Information("Service published event in bus");
         }
 
@@ -31,12 +40,15 @@ namespace HiQoDataGenerator.Scheduler.Services
             Log.Information("Service stopped");
 
             _timer?.Change(Timeout.Infinite, 0);
+            Dispose();
+
             return Task.CompletedTask;
         }
 
         public void Dispose()
         {
             _timer?.Dispose();
+            _bus.Dispose();
         }
     }
 }
