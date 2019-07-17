285c0f4dd4a67c4119d5d4ab144ff957e801acc7
Darya <makarenko.darya.work@gmail.com>
2019-07-17 12:44:03 +0300

    fixed elmah logging and exceptions

--- HiQoDataGenerator.Web/LoggingInfrastructure/RedisErrorLog.cs
+++ HiQoDataGenerator.Web/LoggingInfrastructure/RedisErrorLog.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using ElmahCore;
 using Microsoft.Extensions.Options;
+using HiQoDataGenerator.Infrastructure.LoggerExtensions;
 
 namespace HiQoDataGenerator.Web.LoggingInfrastructure
 {
     public class RedisErrorLog : ErrorLog
     {
         private readonly ElmahRedisClient _redisClient;
-
         public RedisErrorLog(IOptions<ElmahOptions> options)
         {
+            LoggerExtensions.CreateLogger("Elmah");
             _redisClient = new ElmahRedisClient(options.Value.ConnectionString);
         }
 
@@ -20,11 +21,15 @@ namespace HiQoDataGenerator.Web.LoggingInfrastructure
         public override string Log(Error error)
         {
             if (error == null)
-                throw new ArgumentNullException(nameof(error));
+            {
+                var paramName = nameof(error);
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {paramName} null reference.");
+                throw new ArgumentNullException(paramName);
+            }
 
             var errorXml = ErrorXml.EncodeString(error);
             var id = Guid.NewGuid();
-            var redisKey = GetRedisKey(id.ToString());
+            var redisKey = GetRedisKey(id);
             var errorLog = new ErrorData
             {
                 Id = id,
@@ -39,11 +44,19 @@ namespace HiQoDataGenerator.Web.LoggingInfrastructure
 
         public override ErrorLogEntry GetError(string id)
         {
+            var paramName = nameof(id);
             if (id == null)
-                throw new ArgumentNullException(nameof(id));
+            {
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {paramName} null reference.");
+                throw new ArgumentNullException(paramName);
+            }
 
             if (id.Length == 0)
-                throw new ArgumentException(null, nameof(id));
+            {
+                var message = $"{paramName} can't be empty.";
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {message}.");
+                throw new ArgumentException(message, paramName);
+            }
 
             Guid errorGuid;
 
@@ -51,19 +64,22 @@ namespace HiQoDataGenerator.Web.LoggingInfrastructure
             {
                 errorGuid = new Guid(id);
             }
-            catch (FormatException e)
+            catch (FormatException exception)
             {
-                throw new ArgumentException(e.Message, nameof(id), e);
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {paramName} invalid format.");
+                throw new ArgumentException(exception.Message, paramName, exception);
             }
 
-            var redisKey = GetRedisKey(errorGuid.ToString());
+            var redisKey = GetRedisKey(errorGuid);
 
             var loggedError = _redisClient.GetData(redisKey);
 
             var errorInfo = loggedError?.Info;
 
             if (errorInfo == null)
+            {
                 return null;
+            }
 
             var error = ErrorXml.DecodeString(errorInfo);
             return new ErrorLogEntry(this, id, error);
@@ -72,17 +88,29 @@ namespace HiQoDataGenerator.Web.LoggingInfrastructure
         public override int GetErrors(int pageIndex, int pageSize, ICollection<ErrorLogEntry> errorEntryCollection)
         {
             if (pageIndex < 0)
-                throw new ArgumentOutOfRangeException(nameof(pageIndex), pageIndex, null);
+            {
+                var paramName = nameof(pageIndex);
+                var message = $"Argument {paramName} can't have negative value!";
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {message}");
+                throw new ArgumentOutOfRangeException(paramName, pageIndex, message);
+            }
 
             if (pageSize < 0)
-                throw new ArgumentOutOfRangeException(nameof(pageSize), pageSize, null);
+            {
+                var paramName = nameof(pageSize);
+                var message = $"Argument {paramName} has negative value.";
+                LoggerExtensions.LogError($"Error in RedisErrorLog: {message}");
+                throw new ArgumentOutOfRangeException(paramName, pageSize, message);
+            }
 
             if (errorEntryCollection == null)
+            {
                 errorEntryCollection = new List<ErrorLogEntry>();
+            }
 
             var allLoggedErrors = _redisClient.GetValues(GetRedisKey("*"));
 
-            var loggedErrors = allLoggedErrors.ToList();
+            var loggedErrors = allLoggedErrors.ToArray();
             foreach (var eachLog in loggedErrors.Skip(pageSize * pageIndex).Take(pageSize))
             {
                 var error = ErrorXml.DecodeString(eachLog.Info);
@@ -92,9 +120,14 @@ namespace HiQoDataGenerator.Web.LoggingInfrastructure
             return loggedErrors.Count();
         }
 
-        private static string GetRedisKey(string id)
+        private static string GetRedisKey(Guid id)
         {
-            return "elmah-error:" + id;
+            return $"elmah-error:{id}";
+        }
+
+        private static string GetRedisKey(string key)
+        {
+            return $"elmah-error:{key}";
         }
     }
 }
