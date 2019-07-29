7dd7f9982dfecc3ba93cd65fa76db05674c625e2
DenisKaminsky <deniskaminsky123@mail.ru>
2019-07-29 14:02:34 +0300

    fix

---
+++ HiQoDataGenerator.DAL/Migrations/20190729105049_Change File Name max length.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HiQoDataGenerator.DAL.Migrations
+{
+    public partial class ChangeFileNamemaxlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "FilesMetadata",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "FilesMetadata",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+        }
+    }
+}
