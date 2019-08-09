8260970de57f675485b091ea243752c837002f91
Serafim Lazuka <serafim.lazuko@gmail.com>
2019-08-09 11:33:21 +0300

    Add user controller and userViewModel

---
+++ HiQoDataGenerator.Web/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HiQoDataGenerator.Core.Interfaces;
+using HiQoDataGenerator.Core.Entities;
+using HiQoDataGenerator.Web.ViewModels;
+
+namespace HiQoDataGenerator.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : RootController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService , IMapperFactory maperFactory) : base(maperFactory)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Gets user by id.
+        /// </summary>
+        /// <returns>Status code 200 and view model.</returns>
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var userModel = await _userService.GetByIdAsync(id);
+            var userViewModel = _mapper.Map<UserViewModel>(userModel);
+
+            return Ok(userViewModel);
+        }
+
+        /// <summary>
+        /// Adds new user.
+        /// </summary>
+        /// <returns>Status code 200 and view model.</returns>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(UserViewModel userViewModel)
+        {
+            var userModel = _mapper.Map<UserModel>(userViewModel);
+
+            await _userService.AddUserAsync(userModel);
+            return Ok();
+        }
+    }
+}
---
+++ HiQoDataGenerator.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HiQoDataGenerator.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string AccessToken { get; set; }
+        public int DataAccessExpirationTime { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
